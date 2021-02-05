f019394343cff4c0fc2f86feda65dc28a62abdf6
Leonardo <luisfonseca9521@gmail.com>
2021-02-05 00:22:16 -0600

    Create index type for reducer

---
+++ FrontEnd/adikka_todo/src/types/index.js
@@ -0,0 +1,6 @@
+export const GET_TASK       = 'GET_TASK';
+export const CURRENT_TASK   = 'CURRENT_TASK';
+export const UPDATE_TASK    = 'UPDATE_TASK';
+export const PATCH_TASK     = 'PATCH_TASK';
+export const DELETE_TASK    = 'DELETE_TASK';
+export const CREATE_TASK    = 'CREATE_TASK';
