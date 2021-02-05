86666b07e257527290663fb026baa8d6f1273e3f
Leonardo <luisfonseca9521@gmail.com>
2021-02-05 01:35:18 -0600

    Add Routing

--- FrontEnd/adikka_todo/src/App.js
+++ FrontEnd/adikka_todo/src/App.js
@@ -1,13 +1,21 @@
 import React, { Fragment } from 'react';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Main from './components/layout/Main';
+import FormEditTask from './components/tasks/FormEditTask';
 import TaskState from './context/tasks/taskState';
 
 function App() {
   return (
     <Fragment>
       <TaskState>
-        <Main />
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Main} />
+            <Route exact path='/detail/:id' component={FormEditTask} />
+          </Switch>
+        </Router>
       </TaskState>
     </Fragment>
   );
