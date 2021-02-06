97ae948f4e204e390289ad2e91ec75a905efdc7b
Leonardo <luisfonseca9521@gmail.com>
2021-02-06 10:02:28 -0600

    Move locations component search

--- FrontEnd/adikka_todo/src/components/tasks/Tasks.js
+++ FrontEnd/adikka_todo/src/components/tasks/Tasks.js
@@ -1,35 +1,18 @@
-import React, { Fragment, useContext, useEffect } from 'react';
+import React, { Fragment } from 'react';
 
-import TaskContext from '../../context/tasks/taskContext';
-
-import FormSearch from './FormSearch';
 import FormTaks from './FormTaks';
 import ListTasks from './ListTasks';
 
-const Tasks = () => {
-    const taskContext = useContext(TaskContext);
-    const { tasks, getTasks } = taskContext;
-    
-    useEffect(() => {
-        if(tasks.length === 0) 
-            getTasks();
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
+const Tasks = ({ tasks }) => {
     return (
         <Fragment>
             <div className='my-3 p-3 bg-white rounded shadow-sm'>
                 <div className="row border-bottom pb-2 mb-0">
-                    <div className='col-md-4'>
+                    <div className='col-md-6'>
                         <h2>Tasks</h2>
                     </div>
 
                     <div className='col-md-6'>
-                        <FormSearch tasks={tasks}/>
-                    </div>
-
-                    <div className='col-md-2'>
                         <button type="button" className="btn btn-light float-end" data-bs-toggle="modal" data-bs-target="#showForm">
                             <i className="fas fa-plus-circle"></i> &nbsp;
                             Add Task
