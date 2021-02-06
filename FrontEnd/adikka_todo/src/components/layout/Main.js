ab87d1706d464e1a4812e6950e4f59ded0d5f525
Leonardo <luisfonseca9521@gmail.com>
2021-02-06 10:03:14 -0600

    Get all items from main component

--- FrontEnd/adikka_todo/src/components/layout/Main.js
+++ FrontEnd/adikka_todo/src/components/layout/Main.js
@@ -1,15 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
+
+import TaskContext from '../../context/tasks/taskContext';
 
 import Header from './Header';
 import Tasks from '../tasks/Tasks';
 
 const Main = () => {
+    const taskContext = useContext(TaskContext);
+    const { tasks, getTasks } = taskContext;
+
+    useEffect(() => {
+        if(tasks.length === 0)
+            getTasks();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+
     return (
         <Fragment>
             <main className='container'>
                 <Header />
 
-                <Tasks />
+                <Tasks
+                    tasks={tasks}
+                />
             </main>
         </Fragment>
     )
