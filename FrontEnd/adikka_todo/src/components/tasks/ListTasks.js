d6b33741eac13212de7e39ecbcf05e2a5aa3fa1f
Leonardo <luisfonseca9521@gmail.com>
2021-02-06 10:03:36 -0600

    Add funcionality for search

--- FrontEnd/adikka_todo/src/components/tasks/ListTasks.js
+++ FrontEnd/adikka_todo/src/components/tasks/ListTasks.js
@@ -1,33 +1,77 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Task from './Task';
 
 const ListTasks = ({ tasks }) => {
+    const [query, setQuery] = useState('');
+
+    const filterTasks = tasks.filter(task => {
+        return task.title.toLowerCase().includes(query);
+    });
+
     return (
         <Fragment>
-            <table className='table table-responsive text-center'>
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Created At</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Completed</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        tasks.length === 0
-                            ? (<tr>No hay Tasks</tr>)
-                            : tasks.map(task => (
-                                <Task
-                                    key={task.id}
-                                    task={task} />
-                            ))
-                    }
-                </tbody>
-            </table>
+            <div className="row">
+                <div className='d-flex'>
+                    <input type="text"
+                        className='form-control me-2'
+                        placeholder='Search Task'
+                        value={query}
+                        onChange={(e) => {
+                            setQuery(e.target.value);
+                            console.log(e.target.value)
+                        }}
+                    />
+                </div>
+            </div>
+
+            {
+                filterTasks.length === 0
+                    ? (
+                        <div className="row">
+                            <table className='table table-responsive text-center'>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Created At</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Completed</th>
+                                        <th scope="col">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>There is not TODOS</tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    )
+                    : (
+                        <div className="row">
+                            <table className='table table-responsive text-center'>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Created At</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Completed</th>
+                                        <th scope="col">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        filterTasks.map(task => (
+                                            <Task
+                                                key={task.id}
+                                                task={task} />
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    )
+            }
         </Fragment>
     )
 }
