1b52befad6cea44a42c6e6188b51f1efd95c0758
Leonardo <luisfonseca9521@gmail.com>
2021-02-10 21:45:27 -0600

    Change for control flow

--- FrontEnd/adikka_todo/src/components/tasks/ListTasks.js
+++ FrontEnd/adikka_todo/src/components/tasks/ListTasks.js
@@ -1,82 +1,19 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import DisplayResult from './DisplayResult';
 
 // Importación de Subcomponentes
-import Task from './Task';
+import NoneResult from './NoneResult';
 
 /* 
  Componente ListTasks principal
 */
-const ListTasks = ({ tasks }) => {
-    // Declaración del state para la manipulacion de busqueda
-    const [query, setQuery] = useState('');
-
-    // Función que manipulara los resultados conforme a la busqueda
-    const filterTasks = tasks.filter(task => {
-        return task.title.toLowerCase().includes(query);
-    });
-
+const ListTasks = ({ filterTasks }) => {
     return (
         <Fragment>
-            <div className="row">
-                <div className='d-flex'>
-                    <input type="text"
-                        className='form-control me-2'
-                        placeholder='Search Task'
-                        value={query}
-                        onChange={(e) => {
-                            setQuery(e.target.value);
-                            console.log(e.target.value)
-                        }}
-                    />
-                </div>
-            </div>
-
             {
                 filterTasks.length === 0
-                    ? (
-                        <div className="row">
-                            <table className='table table-responsive text-center'>
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Created At</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Completed</th>
-                                        <th scope="col">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>There is not TODOS</tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    )
-                    : (
-                        <div className="row">
-                            <table className='table table-responsive text-center'>
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Created At</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Completed</th>
-                                        <th scope="col">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        filterTasks.map(task => (
-                                            <Task
-                                                key={task.id}
-                                                task={task} />
-                                        ))
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
-                    )
+                    ? (<NoneResult />)
+                    : (<DisplayResult filterTasks={filterTasks} />)
             }
         </Fragment>
     )
