00cf8add2a906795d6be0de7d6792ebf11a04e06
Leonardo <luisfonseca9521@gmail.com>
2021-02-10 21:43:41 -0600

    Component flow change

--- FrontEnd/adikka_todo/src/components/tasks/Tasks.js
+++ FrontEnd/adikka_todo/src/components/tasks/Tasks.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 
 // Importación de Subcomponentes
 import FormTaks from './FormTaks';
-import ListTasks from './ListTasks';
+import FormSearch from './FormSearch';
 
 /* 
  Componente Task principal
@@ -10,37 +10,39 @@ import ListTasks from './ListTasks';
 const Tasks = ({ tasks }) => {
     return (
         <Fragment>
-            <div className='my-3 p-3 bg-white rounded shadow-sm'>
-                <div className="row border-bottom pb-2 mb-0">
-                    <div className='col-md-6'>
-                        <h2>Tasks</h2>
-                    </div>
-
-                    <div className='col-md-6'>
-                        <button type="button" className="btn btn-light float-end" data-bs-toggle="modal" data-bs-target="#showForm">
-                            <i className="fas fa-plus-circle"></i> &nbsp;
-                            Add Task
-                        </button>
-                    </div>
-                </div>
-
-                <br />
-
-                <div className="row">
-                    <ListTasks tasks={tasks} />
-                </div>
-            </div>
-
-            <div className="modal fade" id="showForm" data-bs-keyboard="false" aria-labelledby="showFormLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="showFormLabel">Task</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="container-fluid">
+                <div className='my-3 p-3 bg-white rounded shadow-sm'>
+                    <div className="row border-bottom pb-2 mb-0">
+                        <div className='col-md-1'>
+                            <h2>Tasks</h2>
                         </div>
 
-                        <div className="modal-body">
-                            <FormTaks />
+                        <div className="col-md-9">
+                            <FormSearch tasks={tasks} />
+                        </div>
+
+                        <div className='col-md-2'>
+                            <button type="button" className="btn btn-link float-end" data-bs-toggle="modal" data-bs-target="#showForm">
+                                <i className="fas fa-plus-circle"></i> &nbsp;
+                                Add Task
+                            </button>
+                        </div>
+                    </div>
+
+                    <br />
+                </div>
+
+                <div className="modal fade" id="showForm" data-bs-keyboard="false" aria-labelledby="showFormLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="showFormLabel">New Task</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+
+                            <div className="modal-body">
+                                <FormTaks />
+                            </div>
                         </div>
                     </div>
                 </div>
